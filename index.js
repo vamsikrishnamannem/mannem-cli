25882ebcfb244df6e0ce401a40c759d8c2fd5191
vamsi krishna mannem <vamsikrishna_mannem@trimble.com>
2017-10-30 17:19:44 +0530

    add the node -v command

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
 
 var cmd = require('commander');
-if (process.argv[0] === '-v') {
-    console.log("1.0.0");
-    process.exitCode = 0;
-    return;
+var exec = require('child_process').exec;
+cmd.version("1.0.0")
+    .arguments('<cmd>')
+    .parse(process.argv);
+if (typeof task === 'undefined') {
+    console.log("Missing argument");
+    // process.exit(1);
 }
+// console.log('Running ' + task);
+// let command = "npm" + " " + "run" + " " + task;
+// console.log(command);
+
+// exec(command);
+exec('node -v', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});
