4d16e6eb83df7d30b09bca13e95554881cde5398
vamsi krishna mannem <vamsikrishna_mannem@trimble.com>
2017-10-30 15:41:33 +0530

    change the argv

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-var program = require('commander');
-program.arguments('script');
-if (args[0] === '-v') {
+var cmd = require('commander');
+if (process.argv[0] === '-v') {
     console.log("1.0.0");
     process.exitCode = 0;
     return;
